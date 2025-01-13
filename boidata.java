4c97cb63377bc73ae5c097c85b96da759cafc011
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 19:08:04 +0600

    new code

---
+++ boidata.java
@@ -0,0 +1,11 @@
+public class boidata {
+
+    public static void main(String[] args) {
+        System.out.println("Joynto Ghosh\n023940309\nDIU ");// create a new line
+        System.out.println(" 22 \t 3993 "); // create a new tab
+        System.out.println(" 2203 \t 3993 ");
+        System.out.println(" \"Java Programming\" ");// using \" \"
+        System.out.println(" \\Java Programming\\ ");//  using \\ \\ 
+    }
+
+}
