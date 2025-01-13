1e6a93555978d49b03645e0af4e6202b864a8893
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 19:22:10 +0600

    Little changes

--- comment.java
+++ comment.java
@@ -1,8 +1,8 @@
 public class comment {
     public static void main(String[] args) {
-        // First Method
+        // First Method : Single Line comment
         System.out.println("The first method to comment is using // before the statement ");
-        // second Mehtod
+        // second Mehtod : Multi-Line comment
         System.out.println("The second method to comment is using /* */ in the statement ");
         // Third Method "Documentation Comment"
         System.out.println("The Third method to comment is using /** */ in the statement. This is also known as Documentation Comment. ");
