2db4f1e61b188b01f90f855cc6bccac1087d2161
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 18:52:51 +0600

    first commit

---
+++ product.java
@@ -0,0 +1,11 @@
+public class product {
+
+    public static void main(String[] args) {
+        System.out.println("ID:241-45234");
+        System.out.println("Title: Iphone 18pro max");
+        System.out.println("price:120000/-");
+        System.out.println("Discription:This is the leatest apple product");
+        System.out.println("Category:phone");
+    }
+    
+}
