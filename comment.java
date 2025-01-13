bbb130b81bd389f34c9c1ae64db3ccc3d299970e
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 19:18:45 +0600

    Methods of comment in JAVA

---
+++ comment.java
@@ -0,0 +1,11 @@
+public class comment {
+    public static void main(String[] args) {
+        // First Method
+        System.out.println("The first method to comment is using // before the statement ");
+        // second Mehtod
+        System.out.println("The second method to comment is using /* */ in the statement ");
+        // Third Method "Documentation Comment"
+        System.out.println("The Third method to comment is using /** */ in the statement. This is also known as Documentation Comment. ");
+
+    }
+}
