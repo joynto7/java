a6a6c8fc2c03feb79c7a95dce139b18174f509b9
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 20:09:09 +0600

    rules of varible naming in java

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("
+                   1. Start with a Letter or Underscore
+                   2. Variable name  cannot start with a digit.
+                   3. Case Sensitivity.
+                   4. No Reserved Words or KeyWords allowed to use as variable name.
+                   5. Variable name can contain digits(0-9), underscore(_) , dollar sign ($).
+                   6. Variable name can not contain any space.
+
+        ");
+    }
+    
+}
