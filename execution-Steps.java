d275006e3dc9219224e9e48ab456c035c81bc379
joynto7 <joyntoghosh7@gmail.com>
2025-01-13 19:49:05 +0600

    steps of excecution of java

---
+++ execution-Steps.java
@@ -0,0 +1,13 @@
+public class ExecutionSteps {
+    public static void main(String[] args) {
+        System.out.println("
+
+        1) Edit ----Java Statements------>>> |Source-Code(.java)|
+
+        2) Compiler ---- Reads source-code produces---->>>> | Byte-code(.class) |
+
+        3) JVM(Java Virtual Machine)----interpreted bye-code into--->>> | program execution |  
+        
+        ");
+    }
+}
