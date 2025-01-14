8d32d368b5272488858099b781844939ed264136
joynto7 <joyntoghosh7@gmail.com>
2025-01-14 15:31:41 +0600

    Data_type

---
+++ name.java
@@ -0,0 +1,21 @@
+public class name {
+    public static void main(String[] args) {
+
+                 // data_type variable_name
+
+        boolean b = false; // dynamic intialization
+        char c = 'h';
+        short s = 3893;
+        int i = 29393;
+        float f = 476.00f;
+        double d = 30.99;
+
+        // output
+        System.out.println("b = "+ b);
+        System.out.println("c = "+ c);
+        System.out.println("s = "+ s);
+        System.out.println("i = "+ i);
+        System.out.println("f = "+ f);
+        System.out.println("d = "+ d);
+    }
+}
--- varsyntax.java
+++
@@ -1,7 +0,0 @@
-public class varsyntax {
-    
-    public static void main(String[] args) {
-        System.out.println(" variable Syntax : data_type variable_name ");
-    }
-    
-}
