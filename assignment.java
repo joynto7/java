442c747efdf79ae828e6f0ba601648a53d644cd5
joynto7 <joyntoghosh7@gmail.com>
2025-01-14 15:52:18 +0600

    assignment-2

---
+++ assignment.java
@@ -0,0 +1,26 @@
+public class assignment {
+    public static void main(String[] args) {
+        //Declare variables
+        int ID ;
+        String Title ;
+        double price ;
+        String Description;
+        String category;
+
+        //assign the following data in main method
+         ID = 101;
+         Title = "Iphone16";
+         price = 999.00;
+         Description = "best phone with best quality camera";
+         category = "phone";
+
+         //print the data
+
+        System.out.println(ID);
+        System.out.println(Title);
+        System.out.println(price);
+        System.out.println(Description);
+        System.out.println(category);
+        
+    }
+}
