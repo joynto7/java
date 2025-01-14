08429d428df1ae7d77f6c7c46f98cfe2a1e6cdab
joynto7 <joyntoghosh7@gmail.com>
2025-01-14 17:20:16 +0600

    14 feb Project

---
+++ love.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class love{
+  public static void main(String[] args) {
+    
+    // -------->>>>>>>14 Feb Project<<<<<<<<------------
+
+    System.out.println(              "\n\n*****\n ** \n ** \n ** \n*****\n\n*       ********    **      **  ******\n*       **    **     **    **   *         \n*       **    **      **  **    ***      \n*       **    **      **  **    *      \n*****   ********        **      ******               \n\n\n             **        **\n             **        **\n             **        **\n             **        **\n              **      **\n               ** ** **\n\n\n");
+  }
+}
